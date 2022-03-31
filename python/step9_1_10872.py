8dfa590e1c16942a35252445eb909955d9b54b01
wonjin <79980860+woo222@users.noreply.github.com>
2022-04-01 02:54:27 +0900

    Create step9_1_10872.py
    for문 & 재귀

---
+++ python/step9_1_10872.py
@@ -0,0 +1,19 @@
+// 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+// 팩토리얼은 단순 for문으로도 구할 수 있지만, 학습을 위해 재귀를 써 봅시다.
+
+// 단순 for문
+a = int(input())
+fac = 1
+for i in range(1, a+1):
+    fac = fac*i
+print(fac)
+
+// 재귀
+def fac(n):
+    if(n>1):
+        return n * fac(n-1)
+    else:
+        return 1
+
+a = int(input())
+print(fac(a))
