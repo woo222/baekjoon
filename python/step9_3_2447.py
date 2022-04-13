64a9cba943336ea20d777b286a7ecd6c9c37d239
wonjinSong <wjsong0204@naver.com>
2022-04-13 19:09:12 +0900

    upload step9 3 2447

---
+++ python/step9_3_2447.py
@@ -0,0 +1,14 @@
+def square (i, j, N):
+    if((i//N)%3==1 and (j//N)%3==1):
+        print(" ", end='')
+    else:
+        if((N//3)==0):
+            print("*", end='')
+        else:
+            square(i,j,N/3)
+
+n =int(input())
+for i in range (0,n):
+    for j in range(0,n):
+        square(i,j,n)
+    print()
