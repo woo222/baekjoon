ae166962e04327bb2eaae2b2f74ce454118c9ea0
wonjinSong <wjsong0204@naver.com>
2022-10-21 22:45:13 +0900

    1차원 배열

---
+++ python/4344_평균은 넘겠지.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(n):
+    sum = 0
+    count = 0
+    score = list(map(int, input().split()))
+
+    for j in range(score[0]):
+        sum += score[j+1]
+        
+    avg = sum / score[0]
+
+    for j in range(score[0]):
+        if(score[j+1] > avg):
+            count += 1
+
+    result = count / score[0] * 100
+
+    print("{:.3f}%".format(result))
