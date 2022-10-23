f80b97d5ec1694f61453cca129adbc93eb90b4a0
wonjinSong <wjsong0204@naver.com>
2022-10-23 19:25:26 +0900

    리스트 메소드 사용

--- python/10818_최소최대.py
+++ python/10818_최소최대.py
@@ -1,11 +1,9 @@
 n = int(input())
 
 num = list(map(int, input().split()))
-min = max = num[0]
 
-for j in range(n):
-    if(num[j] < min):
-        min = num[j]
-    if(num[j] > max):
-        max = num[j]
-print(min, max)
+for i in range(len(num)):
+    if(i >= n):
+        num.pop()
+
+print(min(num), max(num))
