469107f23d38d015ae3fb82ef908366504490d47
wonjinSong <wjsong0204@naver.com>
2022-10-24 21:08:51 +0900

    파이썬 메소드 사용

--- python/1546_평균.py
+++ python/1546_평균.py
@@ -1,14 +1,9 @@
 n = int(input())
 num = list(map(int, input().split()))
-max = num[0]
-sum = 0
+max_n = max(num)
 
-for i in range(1, n):
-    if(num[i] > max):
-        max = num[i]
+new_n = []
+for i in num:
+    new_n.append(i/max_n*100)
 
-for i in range(n):
-    num[i] = num[i]/max*100
-    sum += num[i]
-
-print(sum/n)
+print(sum(new_n)/n)
