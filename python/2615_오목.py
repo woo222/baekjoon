40048ecbecc6e3b96eca2341f234613bd539d3b3
wonjinSong <wjsong0204@naver.com>
2022-10-29 23:04:52 +0900

    복습필요

--- python/2615_오목.py
+++ python/2615_오목.py
@@ -1,85 +1,37 @@
+import sys
+
 board = []
-baduk = []
 
 for i in range(19):
     board.append(list(map(int, input().split())))
 
-for i in range(19):
-    for j in range(19):
-        if(board[i][j] != 0):
-            baduk.append([board[i][j], i, j])
+dx = [0, 1, 1, -1]
+dy = [1, 0, 1, 1]
 
-count = 0
-for i in range(len(baduk)-1):
-    
-    # 가로 줄이 5개 인지 확인
-    
-    if(baduk[i][1] == baduk[i+1][1] and baduk[i][2]+1 == baduk[i+1][2]):
-        count +=1
-        if baduk[i][0] != baduk[i+1][0]:
-            count = 0
-        
-        if count == 4:
-            result = [baduk[i][0], baduk[i][1]+1, baduk[i][2]-2]
-            break
-    else: count = 0
+for i in range(len(board)):
+    for j in range(len(board[i])):
+        if board[i][j] != 0:
+            omok = board[i][j]
 
-# 우 대각선 아래
-count = 0
-for i in range(len(baduk)):
-    new_baduk = baduk[i]
-    for j in range(len(baduk)):
-        if i == j :
-            continue
-        if(baduk[i][1]+1 == baduk[j][1] and baduk[i][2]+1 == baduk[j][2]):
-            count += 1
+            for k in range(4):
+                cnt = 1
+                x = i + dx[k]
+                y = j + dy[k]
 
-            
-            if(baduk[i][0] != baduk[j][0]):
-                count = 0
+                while 0 <= x < 19 and 0 <= y < 19 and board[x][y] == omok:
+                    cnt += 1
 
-            if(baduk[i][1]==17 and baduk[j][1] ==18):
-                count+=1
-    
-            if count == 5:
-                result = [baduk[i][0], baduk[i][1]-2, baduk[i][2]-2]
-                break
+                    if cnt == 5:
+                        if 0 <= i - dx[k] < 19 and 0 <= j - dy[k] < 19 and board[i - dx[k]][j - dy[k]] == omok:
+                            break
+                        if 0 <= x + dx[k] < 19 and 0 <= y + dy[k] < 19 and board[x + dx[k]][y + dy[k]] == omok:
+                            break
+                        
+                        print(omok)
+                        print(i + 1, j + 1)
+                        sys.exit(0)
 
-# 좌 대각선 아래
-count = 0
-for i in range(len(baduk)-1):
-    new_baduk = baduk[i]
-    for j in range(len(baduk)-1):
-        if i == j :
-            continue
+                    x += dx[k]
+                    y += dy[k]
 
-        if(baduk[i][1]+1 == baduk[j][1] and baduk[i][2]-1 == baduk[j][2]):
-            count += 1
-            
-            if(baduk[i][0] != baduk[j][0]):
-                count = 0
-
-            if count == 4:
-                result = [baduk[i][0], baduk[i][1]+2, baduk[i][2]]
-                break
-
-baduk.sort(key= lambda x:x[2])
-count = 0
-for i in range(len(baduk)-1):
-    for j in range(len(baduk)-1):
-        if i == j:
-            continue
-
-        # 세로 줄이 5개 인지 확인
-        if(baduk[i][2] == baduk[j][2] and baduk[i][1]+1 == baduk[j][1]):
-            count += 1
-            
-            if baduk[i][0] != baduk[j][0]:
-                count = 0
-        
-            if count == 4:
-                result = [baduk[i][0], baduk[i][1]-1, baduk[i][2]+1]
-                break
-
-print(result[0])
-print(result[1], result[2])
+print(0)
