163e98cfb844699a292f3fa8e1e3b18f5b18df98
wonjinSong <wjsong0204@naver.com>
2022-11-07 16:56:41 +0900

    dfs bfs 구현

---
+++ python/그래프/s2_1260_DFS와 BFS.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+lst = [[] for _ in range(n+1)]
+visit_d = [0] * (n+1)
+dfs_q = []
+
+for i in range(m):
+    a, b = map(int, input().split())
+    
+    lst[a].append(b)
+    lst[b].append(a)
+# 각 요소들 정렬
+for i in range(1, n+1):
+    lst[i].sort()
+    
+def dfs(start):
+    visit_d[start] = 1
+    print(start, end=' ')
+
+    for i in lst[start]:
+        if(visit_d[i] == 0):
+            dfs(i)
+
+def bfs(start):
+    dfs_q = deque([start])
+    visit_b = [0] * (n+1)
+
+    visit_b[start] = 1
+
+    while(dfs_q):
+        find = dfs_q.popleft()
+
+        print(find, end=' ')
+
+        for i in lst[find]:
+            if(visit_b[i] == 0):
+                dfs_q.append(i)
+                visit_b[i] = 1
+        
+dfs(v)
+print()
+bfs(v)
