6e56737e75f209e2068475e76c17e63ce14823fe
wonjinSong <wjsong0204@naver.com>
2022-11-10 00:16:42 +0900

    dfs 스택 사용

--- python/그래프/s2_24479_알고리즘 수업 깊이 우선 탐색1.py
+++ python/그래프/s2_24479_알고리즘 수업 깊이 우선 탐색1.py
@@ -1,27 +1,36 @@
 import sys
-sys.setrecursionlimit(10**6)        # 파이썬의 최대 재귀 깊이를 크게 변경
-
-n, m, r = map(int, sys.stdin.readline().split())
+n, m, r = map(int, input().split())
 dfs_l = [[] for _ in range(n+1)]    # 연결된 정점 배열
-visit = [0] * (n+1)                 # 방문한 정점 배열
-cnt = 1                             # 몇 번 째 방문 했는지 확인
 
 for i in range(m):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, input().split())
     dfs_l[a].append(b)
     dfs_l[b].append(a)
 
-def dfs(R):
-    global cnt
-    visit[R] = cnt
-    dfs_l[R].sort()
-    for i in dfs_l[R]:
-        if visit[i] == 0:
-            cnt += 1
-            dfs(i)
-
-
-dfs(r)
-
 for i in range(1, n+1):
-    print(visit[i])
+    dfs_l[i].sort(reverse=True)
+
+def dfs(R):
+    stack = [r]
+    visit = [0] * (n+1)                 # 방문 여부 배열
+    result = [0] * (n+1)                # 방문 순서
+    cnt = 1                             # 몇 번 째 방문 했는지 확인
+
+    while stack:
+        node = stack.pop()
+        if visit[node] == 1:
+            continue
+        visit[node] = 1
+        result[node] = cnt
+        cnt += 1
+
+        for i in dfs_l[node]:
+            if visit[i] == 0:
+                stack.append(i)
+
+    return result
+
+rst = dfs(r)
+
+for i in rst[1:]:
+    print(i)
