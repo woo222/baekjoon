054765bd62f8eb627e7a8a814b775cfc30361748
wonjinSong <wjsong0204@naver.com>
2022-11-15 18:59:37 +0900

    bfs 방문순서

---
+++ python/그래프/s2_24444_알고리즘 수업 너비 우선 탐색.py
@@ -0,0 +1,34 @@
+from collections import deque
+n, m, r = map(int, input().split())
+
+bList = [[] for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    bList[a].append(b)
+    bList[b].append(a)
+
+for i in range(1, n+1):
+    bList[i].sort()
+
+def bfs(start):
+    bfs_q = deque([start])
+    visit = [0] * (n+1)
+
+    visit[start] = 1
+    cnt = 2
+
+    while(bfs_q):
+        node = bfs_q.popleft()
+
+        for i in bList[node]:
+            if(visit[i] == 0):
+                bfs_q.append(i)
+                visit[i] = cnt
+                cnt += 1
+    return visit
+
+rst = bfs(r)
+
+for i in range(1, n+1):
+    print(rst[i])
