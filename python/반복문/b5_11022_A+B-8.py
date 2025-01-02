2f051b30a5b5e0c0fc4233b693d3bf0c2d4beea4
wonjinSong <wjsong0204@naver.com>
2025-01-02 22:14:22 +0900

    반복문

---
+++ python/반복문/b5_11022_A+B-8.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+t = int(stdin.readline().strip())
+
+for i in range(1, t+1):
+    a, b = map(int, stdin.readline().split())
+
+    print("Case #" +  str(i) + ":", a, '+', b, '=', a+b)
